cb12ed30be32c1565ba05c617d3cb82baa21dc03
Matheus Oliveira <matheus.vetor@gmail.com>
2023-10-10 18:50:35 -0300

    factory things

--- spec/factories/municipes.rb
+++ spec/factories/municipes.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :municipe do
     name { Faker::Name.name }
     cpf { Faker::IDNumber.brazilian_citizen_number }
-    cns { Faker::IDNumber.brazilian_citizen_number }
+    cns { CnsBrazil::Cns.generate }
     email { Faker::Internet.email }
     phone { Faker::PhoneNumber.cell_phone }
-    birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    birth_date_string { Faker::Date.birthday(min_age: 18, max_age: 65).strftime('%d/%m/%Y') }
     active { [true, false].sample }
   end
 end
