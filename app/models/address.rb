6b6d63c4ec2acb968ec4568104248eac08be67be
Matheus Oliveira <matheus.vetor@gmail.com>
2023-10-10 18:49:32 -0300

    new validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
-  validates :cep, presence: true
-  validates :line1, presence: true
+  validates :zip_code, presence: true
+  validates :street, presence: true
+  validates :number, presence: true
   validates :neighborhood, presence: true
   validates :city, presence: true
   validates :state, presence: true
